f0faf2d26e3b8af02894d688d780812a29c3d019
LinkunGao <gaolinkun123@gmail.com>
2024-09-23 17:36:36 +1200

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
         '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/DigitalTWINonFHIRFrontend/",
+  // base: "/DigitalTWINonFHIRFrontend/",
   build: {
     outDir: "./build",
     minify: "terser",
